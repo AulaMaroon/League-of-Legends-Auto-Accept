f30379b6322fb93ae6b9f43dc7d8155da85f9300
AulaMaroon <kadekfp@gmail.com>
2022-01-02 17:33:43 +0800

    Added Window Detection

---
+++ AutoAccept(old).py
@@ -0,0 +1,82 @@
+import pyautogui
+import keyboard
+import os, ctypes
+import time
+
+try:
+ is_admin = os.getuid() == 0
+except AttributeError:
+ is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+
+if(is_admin == False):
+    print('Please Run Script as Administrator')
+    exit()
+
+def find():
+    os.system('cls')
+    checker()
+    iq = pyautogui.locateOnScreen('./image/inq.png', confidence = 0.8)
+    if iq != None:
+        accept()
+    f = pyautogui.locateOnScreen('./image/fbutton.png', confidence = 0.8)
+    pyautogui.doubleClick(f)
+    accept()
+
+def accept():
+    a = None
+    print ('Waiting For Queue')
+    print ()
+    print ('Press Escape To Cancel Queue')
+    while(a == None):
+        a = pyautogui.locateOnScreen('./image/accept.png', grayscale = True, confidence = 0.8)
+        if (a != None): 
+            pyautogui.doubleClick(a)
+        if keyboard.is_pressed('esc'):
+            cancel()
+        b = pyautogui.locateOnScreen('./image/emote.png', confidence = 0.9)
+        if (b != None):
+            exit()
+    
+def cancel():
+    c = pyautogui.locateOnScreen('./image/cancel.png', confidence = 0.9)
+    pyautogui.doubleClick(c)
+    os.system('cls')
+    wait()
+
+def wait():
+    logo()
+    print('- Press Enter To Start Queue')
+    print('- Press ESC To Close Program')
+    wait = 0
+    while(wait == 0):
+        if keyboard.is_pressed('enter'):
+            os.system('cls')
+            find()
+        if keyboard.is_pressed('esc'):
+            os.system('cls')
+            exit()
+
+def logo():
+    os.system('cls')
+    print("""                _       __  __                             
+     /\        | |     |  \/  |                            
+    /  \  _   _| | __ _| \  / | __ _ _ __ ___   ___  _ __  
+   / /\ \| | | | |/ _` | |\/| |/ _` | '__/ _ \ / _ \| '_ \ 
+  / ____ \ |_| | | (_| | |  | | (_| | | | (_) | (_) | | | |
+ /_/    \_\__,_|_|\__,_|_|  |_|\__,_|_|  \___/ \___/|_| |_|
+                                                           
+                                                           """ )
+    #print("-----------------------------------------------------------")
+    return()
+
+
+def checker():
+    cm = pyautogui.locateOnScreen('./image/cmi.png', confidence=0.8)
+    pt = pyautogui.locateOnScreen('./image/party.png', confidence=0.8)
+    if (pt != None):
+        if (cm == None):
+            pyautogui.doubleClick(pt)
+            time.sleep(1.5) 
+    return()
+
+wait()
--- AutoAccept.py
+++ AutoAccept.py
@@ -1,60 +1,74 @@
+import pygetwindow
 import pyautogui
 import keyboard
-import os, ctypes
+import os,ctypes
+import numpy as np
 import time
 
-try:
- is_admin = os.getuid() == 0
-except AttributeError:
- is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
 
-if(is_admin == False):
-    print('Please Run Script as Administrator')
-    exit()
+l, t, w, h = (0,0,0,0)
 
-def find():
-    os.system('cls')
-    checker()
-    iq = pyautogui.locateOnScreen('./image/inq.png', confidence = 0.8)
-    if iq != None:
-        accept()
-    f = pyautogui.locateOnScreen('./image/fbutton.png', confidence = 0.8)
-    pyautogui.doubleClick(f)
-    accept()
-
-def accept():
-    a = None
-    print ('Waiting For Queue')
-    print ()
-    print ('Press Escape To Cancel Queue')
-    while(a == None):
-        a = pyautogui.locateOnScreen('./image/accept.png', grayscale = True, confidence = 0.8)
-        if (a != None): 
-            pyautogui.doubleClick(a)
-        if keyboard.is_pressed('esc'):
-            cancel()
-        b = pyautogui.locateOnScreen('./image/emote.png', confidence = 0.9)
-        if (b != None):
-            exit()
-    
-def cancel():
-    c = pyautogui.locateOnScreen('./image/cancel.png', confidence = 0.9)
-    pyautogui.doubleClick(c)
-    os.system('cls')
-    wait()
+def game_window_loc():
+    global l, t, w, h 
+    Game_Window = pygetwindow.getWindowsWithTitle('League of Legends')[0]
+    l, t, w, h = Game_Window.left, Game_Window.top, Game_Window.width, Game_Window.height
+    return
 
 def wait():
     logo()
     print('- Press Enter To Start Queue')
     print('- Press ESC To Close Program')
-    wait = 0
-    while(wait == 0):
+    while True:
         if keyboard.is_pressed('enter'):
             os.system('cls')
             find()
         if keyboard.is_pressed('esc'):
             os.system('cls')
             exit()
+
+def find():
+    os.system('cls')
+    checker()
+    game_window_loc()
+    in_queue = pyautogui.locateOnScreen('./image/inq.png', confidence = 0.8, region = (l, t, w, h))
+    if in_queue != None:
+        accept()
+    find_button = pyautogui.locateOnScreen('./image/fbutton.png', confidence = 0.8, region = (l, t, w, h))
+    if find_button != None:
+        pyautogui.click(find_button)
+        accept()
+
+def cancel():
+    game_window_loc()
+    cancel_queue = pyautogui.locateOnScreen('./image/cancel.png', confidence = 0.8, region = (l, t, w, h))
+    pyautogui.click(cancel_queue)
+    os.system('cls')
+    wait()
+
+def accept():
+    print('Waiting For Queue')
+    print()
+    print('Press Escape To Cancel Queue')
+    while True:
+        game_window_loc()
+        accept_queue = pyautogui.locateOnScreen('./image/accept.png', confidence = 0.8, region = (l, t, w, h))
+        if (accept_queue != None):
+            pyautogui.click(accept_queue)
+        if keyboard.is_pressed('esc'):
+            cancel()
+        close_auto_accept = pyautogui.locateOnScreen('./image/emote.png', confidence = 0.8, region = (l, t, w, h))
+        if (close_auto_accept != None):
+            exit()
+
+def checker():
+    game_window_loc()
+    change_mode = pyautogui.locateOnScreen('./image/cmi.png', confidence = 0.8, region = (l, t, w, h))
+    party_image = pyautogui.locateOnScreen('./image/party.png', confidence = 0.8, region = (l, t, w, h))
+    if (party_image !=None):
+        if (change_mode == None):
+            pyautogui.click(party_image)
+            time.sleep(1.5)
+    return
 
 def logo():
     os.system('cls')
@@ -66,17 +80,6 @@ def logo():
  /_/    \_\__,_|_|\__,_|_|  |_|\__,_|_|  \___/ \___/|_| |_|
                                                            
                                                            """ )
-    #print("-----------------------------------------------------------")
-    return()
-
-
-def checker():
-    cm = pyautogui.locateOnScreen('./image/cmi.png', confidence=0.8)
-    pt = pyautogui.locateOnScreen('./image/party.png', confidence=0.8)
-    if (pt != None):
-        if (cm == None):
-            pyautogui.doubleClick(pt)
-            time.sleep(1.5) 
-    return()
+    return
 
 wait()
